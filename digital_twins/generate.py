57d8fe98411a27864e98bd9c0f845583a42e91bd
Peter Rydberg <hmp.rydberg@gmail.com>
2020-09-07 12:54:06 +0200

    Create 1000 shell users

--- digital_twins/generate.py
+++ digital_twins/generate.py
@@ -1,8 +1,28 @@
+from typing import List
+import uuid
+from uuid import UUID
+
+
 class User:
-    def __init__(self, age: int):
+    def __init__(self, age: int, gender: str):
+        self.uuid: UUID = uuid.uuid4()
         self.age: int = age
+        self.gender: str = gender
+
+
+def generate_users(amount: int = 1000):
+    users: List[User] = []
+
+    for _ in range(amount):
+        users.append(generate_random_user())
+
+    return users
+
+
+def generate_random_user() -> User:
+    return User(1, "male")
 
 
 if __name__ == "__main__":
-    user: User = User(age=15)
-    print(user.age)
+    users = generate_users()
+    print(users[0].__dict__)
