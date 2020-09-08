5d68654f3f6151cf67266c9bd9aa103bc9a01c0e
Peter Rydberg <hmp.rydberg@gmail.com>
2020-09-08 17:33:43 +0200

    Added chronic diseases

--- digital_twins/generate.py
+++ digital_twins/generate.py
@@ -1,19 +1,21 @@
-from random import choice, randint
+from random import choice, randint, random, sample
 from typing import List, Generic, Dict, TypeVar
 from uuid import UUID
 from datetime import datetime
 
 import uuid
 
-# Example: {
-#   "lungscans":
-#       {
-#           "created": "2020-09-07 13:47:19.794394",
-#           "lastchanged": "2020-09-07 13:47:19.794394",
-#           "value": "https://www.somepointer.com/id"
-#       }
-# }
+# From https://www.medicalschemes.com/medical_schemes_pmb/chronic_disease_list.htm
+chronic_diseases: List[str] = [
+    "Addison's disease", "Asthma", "Bronchiectasis", "Cardiac failure", "Cardiomyopathy",
+    "Chronic obstructive pulmonary disorder", "Chronic renal disease  ", "Coronary artery disease", "Crohn's disease  ",
+    "Diabetes insipidus", "Diabetes mellitus types 1 & 2", "Dysrhythmias  ", "Epilepsy", "Glaucoma  ", "Haemophilia",
+    "Hyperlipidaemia", "Hypertension", "Hypothyroidism", "Multiple sclerosis", "Parkinson's disease",
+    "Rheumatoid arthritis", "Schizophrenia", "Systemic lupus erythematosus", "Ulcerative colitis",
+    "Bipolar Mood Disorder"]
 
+
+# Types
 T = TypeVar('T')
 
 
@@ -28,7 +30,7 @@ class Appendix(Generic[T]):
         self.lastchanged = datetime.now()
 
 
-class Appendices(Generic[T]):
+class Appendices():
     def __init__(self) -> None:
         self.appendices: Dict[str, Appendix[T]] = {}
         self.lastchanged: datetime = datetime.now()
@@ -45,13 +47,28 @@ class Appendices(Generic[T]):
         del self.appendices[key]
         self.lastchanged = datetime.now()
 
+# Example: {
+#   "lungscans":
+#       {
+#           "created": "2020-09-07 13:47:19.794394",
+#           "lastchanged": "2020-09-07 13:47:19.794394",
+#           "value": "https://www.somepointer.com/id"
+#       }
+# }
+
 
 class User:
-    def __init__(self, age: int, sex: str, appendices: Appendices[T] = Appendices()) -> None:
+    def __init__(self,
+                 age: int,
+                 sex: str,
+                 conditions: List[str] = [],
+                 appendices: Appendices = Appendices()
+                 ) -> None:
         self.uuid: UUID = uuid.uuid4()
         self.age: int = age
         self.sex: str = sex
-        self.appendices: Appendices[T] = appendices
+        self.conditions: List[str] = conditions
+        self.appendices: Appendices = appendices
 
 
 def generate_users(amount: int = 1000) -> List[User]:
@@ -66,8 +83,9 @@ def generate_users(amount: int = 1000) -> List[User]:
 def generate_random_user() -> User:
     age: int = randint(0, 120)
     sex: str = choice(["male", "female"])
+    conditions: List[str] = sample(chronic_diseases, 1 if random() > 0.97 else 0)
 
-    return User(age, sex)
+    return User(age, sex, conditions)
 
 
 if __name__ == "__main__":
@@ -75,4 +93,5 @@ if __name__ == "__main__":
     users[0].appendices.add("head", "owie")
     users[0].appendices.add("knee", "ouch")
     users[0].appendices.delete("knee")
-    print(users[0].appendices.appendices)
+    print(users[0])
+    print("Amount of users with chronic diseases is", sum(1 for user in users if user.conditions != []))
