5c8dc197baddb7b87db92c69b000e37cae9929b6
Peter Rydberg <hmp.rydberg@gmail.com>
2020-11-05 16:17:35 +0100

    Updated workspace config

--- knowledge_generation_engine/run_docker.sh
+++ knowledge_generation_engine/run_docker.sh
@@ -2,17 +2,16 @@
 #export dockerImage=nvcr.io/nvidia/clara-train-sdk:v3.0
 #docker run -it --rm --gpus all --shm-size=1G --ulimit memlock=-1 --ulimit stack=67108864 -v /home/hmrydber/Prosjekter/master:/workspace/master $dockerImage /bin/bash
 
-
 export NVIDIA_RUNTIME="--runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0"
 export OPTIONS="--shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864"
-export SOURCE_DIR=/home/hmrydber/Prosjekter/master
-export MOUNT_DIR=/aiaa-experiments
+export SOURCE_DIR=/home/hmrydber/Prosjekter/master/knowledge_bank/models/prostate
+export MOUNT_DIR=/knowledge_bank/models/prostate
 export LOCAL_PORT=9000
 export REMOTE_PORT=80
 export DOCKER_IMAGE=nvcr.io/nvidia/clara-train-sdk:v3.0
 
 docker run $NVIDIA_RUNTIME $OPTIONS -it --rm \
-       -p $LOCAL_PORT:$REMOTE_PORT \
-       -v $SOURCE_DIR:$MOUNT_DIR \
-       $DOCKER_IMAGE \
-       /bin/bash
+-p $LOCAL_PORT:$REMOTE_PORT \
+-v $SOURCE_DIR:$MOUNT_DIR \
+$DOCKER_IMAGE \
+/bin/bash
