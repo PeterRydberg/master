79d103c7ea2ccb02d19e1046b25cbeebe590c1b9
Peter Rydberg <hmp.rydberg@gmail.com>
2020-11-10 16:55:51 +0100

    Maybe fix some permission bug

--- knowledge_generation_engine/clara/export_model.sh
+++ knowledge_generation_engine/clara/export_model.sh
@@ -2,4 +2,6 @@ export MODEL_TYPE=prostate
 export MODEL_NAME=clara_train_mri_prostate_cg_and_pz_automl_v1
 
 mkdir -p /master/knowledge_bank/exported_models/$MODEL_TYPE/$MODEL_NAME/
+chmod 755 /master/knowledge_bank/exported_models/$MODEL_TYPE/$MODEL_NAME/
+chmod 644 /master/knowledge_generation_engine/clara/$MODEL_TYPE/$MODEL_NAME/{models/model.fzn.pb,models/model.trt.pb,config/config_aiaa.json}
 cp /master/knowledge_generation_engine/clara/$MODEL_TYPE/$MODEL_NAME/{models/model.fzn.pb,models/model.trt.pb,config/config_aiaa.json} /master/knowledge_bank/exported_models/$MODEL_TYPE/$MODEL_NAME/
