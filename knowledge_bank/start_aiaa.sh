f86dd3a17af3bc2c6671dad238e6f039ae2d1bf8
Peter Rydberg <hmp.rydberg@gmail.com>
2020-11-10 18:22:38 +0100

    Add debug flag

--- knowledge_bank/start_aiaa.sh
+++ knowledge_bank/start_aiaa.sh
@@ -3,4 +3,4 @@
 export MODEL_TYPE=prostate
 export MODEL_NAME=clara_train_mri_prostate_cg_and_pz_automl_v1
 
-start_aas.sh --workspace /master/knowledge_bank/exported_models/$MODEL_TYPE/$MODEL_NAME
+start_aas.sh --workspace /master/knowledge_bank/exported_models/$MODEL_TYPE/$MODEL_NAME --debug true
