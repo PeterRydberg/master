2b296d2af910c032f81d040abed462bf2d8b96b8
Peter Rydberg <hmp.rydberg@gmail.com>
2020-11-10 15:43:06 +0100

    Some fixes maybe?

--- knowledge_bank/start_aiaa.sh
+++ knowledge_bank/start_aiaa.sh
@@ -3,4 +3,5 @@
 export MODEL_TYPE=prostate
 export MODEL_NAME=clara_train_mri_prostate_cg_and_pz_automl_v1
 
-start_aas.sh --workspace /master/knowledge_bank/exported_models/$MODEL_TYPE/$MODEL_NAME
+./../run_docker.sh \
+    start_aas.sh --workspace /master/knowledge_bank/exported_models/$MODEL_TYPE/$MODEL_NAME
--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@ export NVIDIA_RUNTIME="--runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0"
 export OPTIONS="--shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864"
 export SOURCE_DIR=/home/hmrydber/Prosjekter/master
 export MOUNT_DIR=/master
-export LOCAL_PORT=5000
+export LOCAL_PORT=9000
 export REMOTE_PORT=80
 export DOCKER_IMAGE=nvcr.io/nvidia/clara-train-sdk:v3.0
 
