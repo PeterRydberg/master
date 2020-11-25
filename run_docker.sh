50cb23d79b5c704268581b021caea9964df4a84a
Peter Rydberg <hmp.rydberg@gmail.com>
2020-11-25 15:30:48 +0100

    Run docker as user for better permissions

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,7 @@ export OPTIONS="--shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864"
 export SOURCE_DIR=/home/hmrydber/Prosjekter/master
 export MOUNT_DIR=/master
 export LOCAL_PORT=9000
-export REMOTE_PORT=80
+export REMOTE_PORT=5000
 export DOCKER_NAME=aiaa_server
 export DOCKER_IMAGE=nvcr.io/nvidia/clara-train-sdk:v3.1
 
@@ -14,5 +14,6 @@ docker run $NVIDIA_RUNTIME $OPTIONS -it --rm \
 -v $SOURCE_DIR:$MOUNT_DIR \
 -w $MOUNT_DIR \
 --name $DOCKER_NAME \
+--user "$(id -u):$(id -g)" -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group \
 $DOCKER_IMAGE \
 /bin/bash
