3f33b81ac002ba455e7bcdd03f4d473c3846df84
Peter Rydberg <hmp.rydberg@gmail.com>
2021-01-18 18:07:23 +0100

    Set up basic experiments

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from components.Ecosystem import Ecosystem
 
 
-if __name__ == "__main__":
+def experiment_1():
     eco = Ecosystem()
+
+    # eco.knowledge_generation_engine.add_pretrained_model("prostate", "clara_train_mri_prostate_cg_and_pz_automl", "1")
+    # eco.knowledge_bank.add_model_to_aiaa_server("prostate", "clara_train_mri_prostate_cg_and_pz_automl", "0.0.0.0:80")
+    # eco.knowledge_generation_engine.train_virtual_register_batch(
+    #     image_type="prostate",
+    #     task_type="segmentation",
+    #     model=None,
+    #     batch=None,
+    #     finetune=True,
+    #     gpu="",
+    #     update_batch=False
+    # )
+
+
+def experiment_2():
+    pass
+
+
+def experiment_3():
+    pass
+
+
+if __name__ == "__main__":
+    experiment_1()
+
+    # eco = Ecosystem()
 
     # eco.digital_twin_population.generate_new_population(size=100)
     # eco.knowledge_generation_engine.update_virtual_register(image_type="prostate")
