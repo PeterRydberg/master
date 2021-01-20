32e5b626ff644aff250b88a22c7734f5d91ad9bf
Peter Rydberg <hmp.rydberg@gmail.com>
2021-01-20 16:41:13 +0100

    Added MMAR example for cloning purposes

---
+++ components/knowledge_generation_engine/clara/mmar/commands/set_env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export PYTHONPATH="$PYTHONPATH:/home/kevin/workspace/nvidia/codecurrent/ai-infra/medical/nvmidl/src"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export MMAR_ROOT=${DIR}/..
