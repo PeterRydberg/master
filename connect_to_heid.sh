3fbace0c97352436da663bc2d98888adbec572f4
Peter Rydberg <hmp.rydberg@gmail.com>
2021-01-21 16:24:26 +0100

    Add some GPU flags to scripts

--- connect_to_heid.sh
+++ connect_to_heid.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-srun -N1 -n1 -c2 --mem-per-cpu=1024 --partition=HEID --gres=gpu:1 -w heid --pty bash
+while getopts t:g: flag; do
+    case "${flag}" in
+    t) TIME_LIMIT=${OPTARG} ;;  # e.g. 12:00:00
+    g) GPUS=${OPTARG} ;;  # gpu:2
+    esac
+done
+
+srun -N1 -n1 -c2 --mem-per-cpu=1024 --partition=HEID --gres=$GPUS --time=$TIME_LIMIT -w heid --pty bash
