33eceb5ea13c973c575a733fead2aa9529cbea8a
Peter Rydberg <hmp.rydberg@gmail.com>
2021-02-17 18:08:23 +0100

    Added a bunch of new scripts

---
+++ docker_fed.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while getopts g: flag; do
+    case "${flag}" in
+    g) GPUS=${OPTARG} ;;  # e.g. 2
+    esac
+done
+
+
+export NVIDIA_RUNTIME="--runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0"
+export OPTIONS="--shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864"
+export SOURCE_DIR=/data/hmrydber/master
+export MOUNT_DIR=/master
+export DOCKER_IMAGE=nvcr.io/nvidia/clara-train-sdk:v3.1
+export DOCKER_NAME=aiaa_federate_learning
+export LOCAL_PORT=9002
+export REMOTE_PORT=8002
+export LOCAL_PORT_ADM=9003
+export REMOTE_PORT_ADM=8003
+
+docker run $NVIDIA_RUNTIME $OPTIONS -it --rm \
+-p $LOCAL_PORT:$REMOTE_PORT \
+-p $LOCAL_PORT_ADM:$REMOTE_PORT_ADM \
+-v $SOURCE_DIR:$MOUNT_DIR \
+-w $MOUNT_DIR \
+--gpus $GPUS \
+--name $DOCKER_NAME \
+$DOCKER_IMAGE \
+/bin/bash
---
+++ docker_tensorboard.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while getopts g: flag; do
+    case "${flag}" in
+    g) GPUS=${OPTARG} ;;  # e.g. 2
+    esac
+done
+
+
+export NVIDIA_RUNTIME="--runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0"
+export OPTIONS="--shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864"
+export SOURCE_DIR=/data/hmrydber/master
+export MOUNT_DIR=/master
+export DOCKER_IMAGE=nvcr.io/nvidia/clara-train-sdk:v3.1
+export DOCKER_NAME=aiaa_tensorboard
+export LOCAL_PORT=9001
+export REMOTE_PORT=6006
+
+docker run $NVIDIA_RUNTIME $OPTIONS -it --rm \
+-p $LOCAL_PORT:$REMOTE_PORT \
+-v $SOURCE_DIR:$MOUNT_DIR \
+-w $MOUNT_DIR \
+--gpus $GPUS \
+--name $DOCKER_NAME \
+--user "$(id -u):$(id -g)" -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group \
+$DOCKER_IMAGE \
+/bin/bash
