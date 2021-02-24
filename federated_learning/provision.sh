23091a85b6c5518a8bd44b537968a77c0c630a67
Peter Rydberg <hmp.rydberg@gmail.com>
2021-02-24 17:30:41 +0100

    Fixed silly mistake

--- federated_learning/provision.sh
+++ federated_learning/provision.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp authz_config.json /opt/nvidia/medical/tools/authz_config.json && cp project.yml /opt/nvidia/medical/project.yml && provision
+cp authz_config.json /opt/nvidia/medical/tools/authz_config.json && cp project.yml /opt/nvidia/medical/tools/project.yml && provision
